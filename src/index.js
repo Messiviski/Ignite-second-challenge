0dcaf9e6944a4f8ecdc0906a381e78352ea98f32
Messias <messiasolimpio@hotmail.com>
2021-03-14 17:05:47 -0300

    Cria todos os middlewares com suas responsabilidades

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) return response.status(404).json({ error: "User does not exists!" });
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if((user.pro === false && user.todos.length < 10) || (user.pro === true)) {
+    return next()
+  };
+
+  return response.status(403).json({ error: "You can't create more todos!" });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: "User does not exists!"})
+  };
+
+  if(!validate(id)){
+    return response.status(400).json({ error: "ID is not valid!"})
+  };
+  
+  const todo = user.todos.find((task) => task.id === id);
+
+  if(!todo){
+    return response.status(404).json({ error: "Task not found!"})
+  };
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if(!user){
+    return response.status(404).json({ error: "User does not exists!" });
+  };
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
